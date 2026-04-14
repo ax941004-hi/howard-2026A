2563eec4790c64894d6e3172850c5e4998639666
ax941004-hi <ax941004@gmail.com>
2026-04-14 09:58:55 +0800

    Add files via upload

---
+++ example2.py
@@ -0,0 +1,4 @@
+from example import Process
+
+User = "我是靜宜大學 資管二A Howard"
+Process(User)
---
+++ firestore/read.py
@@ -0,0 +1,15 @@
+import firebase_admin
+from firebase_admin import credentials, firestore
+
+cred = credentials.Certificate("serviceAccountKey.json")
+firebase_admin.initialize_app(cred)
+
+db = firestore.client()
+
+doc_ref = db.document("靜宜資管2026a/Howard")
+doc = doc_ref.get()
+result = doc.to_dict()
+print("文件內容為：{}".format(result))
+print("姓名："+result.get("name"))
+print("郵件：" + result["mail"])
+print(f"姓名{result["name"]}的老師，他的研究室在{result["lab"]}")
