1f25980d746df77ad604ba8f170f71a00980c155
ax941004-hi <ax941004@gmail.com>
2026-05-26 11:26:15 +0800

    Add files via upload

--- web1.py
+++ web1.py
@@ -91,7 +91,7 @@ def webhook():
         info += result
     elif (action == "input.unknown"):
     info =  req["queryResult"]["queryText"]
-        return make_response(jsonify({"fulfillmentText": info}))
+    return make_response(jsonify({"fulfillmentText": info}))
 
     return make_response(jsonify({"fulfillmentText": "Action 不匹配"}))
 
